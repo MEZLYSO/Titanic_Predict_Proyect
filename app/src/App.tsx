27985009369d637fb4cfbcdde4b56bdb11f36e05
Maury SO <lynuxazura@gmail.com>
2025-10-23 10:36:06 -0600

    Update

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -44,7 +44,7 @@ function App() {
 
   return (
     <>
-      <div className="min-h-screen from-blue-50 to-indigo-100 flex items-center justify-center p-4">
+      <div className="min-h-screen bg-black flex items-center justify-center p-4">
         <div className="bg-white shadow-2xl rounded-2xl p-8 w-full max-w-2xl">
           <h1 className="text-3xl font-bold text-center text-indigo-700 mb-6">
             Predicción de Supervivencia - Titanic 🚢
@@ -184,14 +184,13 @@ function App() {
           )}
 
           {/* Resultado */}
-          {result && (
-            <div className="mt-8 text-center">
+
+            <div className="mt-8 text-center h-20">
               <p className="text-lg text-gray-700 mb-2">Probabilidad de sobrevivir:</p>
-              <div className="text-3xl font-bold text-green-600 bg-green-100 inline-block px-6 py-2 rounded-xl shadow-inner">
-                {result}
+              <div className={`text-3xl font-bold ${parseFloat(result!) >= 50?"text-green-600 bg-green-100":"text-red-600 bg-red-100"} inline-block px-6 py-2 rounded-xl shadow-inner`}>
+                {result?result:"0.0%"}
               </div>
             </div>
-          )}
         </div>
       </div>
     </>
