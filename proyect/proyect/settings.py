2866395f806fb2b95d619512b152ede2c503a795
Maury SO <lynuxazura@gmail.com>
2025-10-25 20:55:21 -0600

    CORS

--- proyect/proyect/settings.py
+++ proyect/proyect/settings.py
@@ -125,4 +125,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS=True
+CORS_ALLOW_ALL_ORIGINS=[
+    'https://titanic-predict-proyect-4sbc.vercel.app'
+]
